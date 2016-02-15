306de14869700ca535a96196021c3acd52acd610
App Academy Student <kithaca@gmail.com>
2016-02-15 15:36:31 -0800

    functioning widget fetches current weather

--- bundle.js
+++ bundle.js
@@ -19775,7 +19775,65 @@
 	});
 	
 	var Weather = React.createClass({
-	  displayName: 'Weather'
+	  displayName: 'Weather',
+	
+	  getInitialState: function () {
+	    return { currentLat: null, currentLon: null };
+	  },
+	
+	  componentDidMount: function () {
+	    var lat, lon;
+	    var that = this;
+	    var coord = window.navigator.geolocation.getCurrentPosition(function (position) {
+	      lat = position.coords.latitude;
+	      lon = position.coords.longitude;
+	      that.setState({ currentLat: lat, currentLon: lon });
+	      that.queryOpenWeather();
+	    });
+	  },
+	
+	  queryOpenWeather: function () {
+	    var request = new XMLHttpRequest();
+	    var that = this;
+	
+	    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat=' + this.state.currentLat + '&lon=' + this.state.currentLon + '&APPID=645c5d39c7603f17e23fcaffcea1a3c1', true);
+	
+	    // request.withCredentials = true;
+	    // request.setRequestHeader('APPID', '645c5d39c7603f17e23fcaffcea1a3c1');
+	
+	    request.onload = function () {
+	      if (request.status >= 200 && request.status < 400) {
+	        // Success!
+	        var resp = request.responseText;
+	        var parsed = JSON.parse(resp);
+	        var desp = parsed.weather[0].description;
+	        var temp = parsed.main.temp - 273;
+	        that.setState({ description: desp, temperature: Math.round(temp).toString() });
+	      } else {
+	        // We reached our target server, but it returned an error
+	        console.log("reached but error");
+	      }
+	    };
+	
+	    request.onerror = function () {
+	      // There was a connection error of some sort
+	      console.log("just bad");
+	    };
+	
+	    request.send();
+	  },
+	
+	  render: function () {
+	    return React.createElement(
+	      'div',
+	      null,
+	      'Description: ',
+	      this.state.description,
+	      ', temperature: ',
+	      this.state.temperature
+	    );
+	  }
+	
 	});
 	
 	module.exports = WeatherClock;
